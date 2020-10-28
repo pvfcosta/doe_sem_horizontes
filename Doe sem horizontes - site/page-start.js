5c1d582e4d1a95e127d74d522821a600b74731dd
1blackbear_ <erickvzp45@gmail.com>
2020-10-28 15:04:34 -0300

    Página princial com menu, adição de animação do scroll e atualização de responsividade. v1

--- Doe sem horizontes - site/page-start.js
+++ Doe sem horizontes - site/page-start.js
@@ -7,3 +7,23 @@ function searchSectionLeave() {
   openSearch.innerHTML = '<p class="text-button-contain">Pesquisar pontos de doação</p>';
 }
 /*--------Input do botão----------*/
+
+/*--------Animação da página----------*/
+const elementos = document.querySelectorAll('[data-anima]')
+const animacaoClass= 'animacao';
+function animaScroll() {
+  const topoPaginaNaJanela= window.pageYOffset+((window.innerHeight*3)/4);
+  elementos.forEach(function(elemento){
+    if (topoPaginaNaJanela > elemento.offsetTop) {
+      elemento.classList.add(animacaoClass);
+    } else {
+      elemento.classList.remove(animacaoClass);
+    }
+  });
+}
+if (elementos.length) {
+  window.addEventListener('scroll',function() {
+    animaScroll();
+  })
+}
+/*--------Animação da página----------*/
